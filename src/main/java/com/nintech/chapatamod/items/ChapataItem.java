72e0e7bfb81c0750fbb8f45201e4441e65bbfcf0
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2024-06-21 01:49:17 -0600

    ora

--- src/main/java/com/nintech/chapatamod/items/ChapataItem.java
+++ src/main/java/com/nintech/chapatamod/items/ChapataItem.java
@@ -10,12 +10,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import static com.nintech.chapatamod.items.CreativeTabInit.addToTab;
+
 
 public class ChapataItem {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ChapataMod.MODID);
 
-    public static final RegistryObject<Item> CHAPATA_BBQ = ITEMS.register("chapata_bbq",
+    public static final RegistryObject<Item> CHAPATA_BBQ = addToTab(ITEMS.register("chapata_bbq",
             () -> new Item(new Item.Properties().stacksTo(16)
                     .food(new FoodProperties.Builder()
                             .nutrition(6)
@@ -23,10 +25,10 @@ public class ChapataItem {
                             .effect(new MobEffectInstance(MobEffects.REGENERATION,200,2),1f)
                             .build())
                     .rarity(Rarity.EPIC)
-            ));
+            )));
 
 
-    public static final RegistryObject<Item> CHAPATA_CHICKEN = ITEMS.register("chapata_chicken",
+    public static final RegistryObject<Item> CHAPATA_CHICKEN = addToTab(ITEMS.register("chapata_chicken",
             () -> new Item(new Item.Properties().stacksTo(16)
                     .food(new FoodProperties.Builder()
                             .nutrition(6)
@@ -34,5 +36,5 @@ public class ChapataItem {
                             .effect(new MobEffectInstance(MobEffects.REGENERATION,200,2),1f)
                             .build())
                     .rarity(Rarity.EPIC)
-            ));
+            )));
 }
--- src/main/java/com/nintech/chapatamod/items/CreativeTabInit.java
+++ src/main/java/com/nintech/chapatamod/items/CreativeTabInit.java
@@ -6,6 +6,10 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -13,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
+@Mod.EventBusSubscriber(modid = ChapataMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class CreativeTabInit {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ChapataMod.MODID);
 
@@ -21,7 +26,7 @@ public class CreativeTabInit {
     public static final RegistryObject<CreativeModeTab> CHAPATA_TAB = TABS.register("chapata_tab",
             ()-> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup.chapata_tab"))
-                    .icon(ChapataItem.CHAPATA.get()::getDefaultInstance)
+                    .icon(ChapataItem.CHAPATA_BBQ.get()::getDefaultInstance)
                     .displayItems((displayParams, output)->{
                        CHAPATA_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get()));
                     })
@@ -30,5 +35,12 @@ public class CreativeTabInit {
     public static <T extends Item> RegistryObject<T> addToTab(RegistryObject<T> itemLike){
         CHAPATA_TAB_ITEMS.add(itemLike);
         return itemLike;
+    }
+
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event){
+        if (event.getTabKey() == CreativeModeTab.){
+
+        }
     }
 }
