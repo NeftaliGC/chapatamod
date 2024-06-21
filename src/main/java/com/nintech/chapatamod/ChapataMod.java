f2ed3749f894051f401cf6ee44add0a09a93a437
NeftaliGC <aliguiacruz@outlook.com>
2024-06-20 21:30:03 -0600

    Inicio

---
+++ src/main/java/com/nintech/chapatamod/ChapataMod.java
@@ -0,0 +1,8 @@
+package com.nintech.chapatamod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ChapataMod.MODID)
+public class ChapataMod {
+    public static final String MODID = "chapatamod";
+}
