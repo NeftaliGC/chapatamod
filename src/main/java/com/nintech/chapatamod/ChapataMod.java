506c8194e9f3237ec598a831efdc35aef3bd83dc
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2024-06-20 23:39:42 -0600

    Cambios

--- src/main/java/com/nintech/chapatamod/ChapataMod.java
+++ src/main/java/com/nintech/chapatamod/ChapataMod.java
@@ -1,8 +1,18 @@
 package com.nintech.chapatamod;
 
+import com.nintech.chapatamod.items.ChapataItem;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ChapataMod.MODID)
 public class ChapataMod {
     public static final String MODID = "chapatamod";
+
+    public ChapataMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ChapataItem.ITEMS.register(bus);
+
+    }
 }
--- src/main/java/com/nintech/chapatamod/items/ChapataItem.java
+++ src/main/java/com/nintech/chapatamod/items/ChapataItem.java
@@ -1,2 +1,27 @@
-package com.nintech.chapatamod.items;public class ChapataItem {
+package com.nintech.chapatamod.items;
+
+import com.nintech.chapatamod.ChapataMod;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.flag.FeatureFlag;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ChapataItem {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ChapataMod.MODID);
+
+    public static final RegistryObject<Item> CHAPATA = ITEMS.register("chapata",
+            () -> new Item(new Item.Properties().stacksTo(16)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(6)
+                            .saturationModifier(0.2f)
+                            .build())
+                    .rarity(Rarity.EPIC)
+
+            ));
 }
