8855dcb6bf67337492b1205ba7519e62038717f8
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2024-06-21 01:29:04 -0600

    cambios

---
+++ src/main/java/com/nintech/chapatamod/items/CreativeTabInit.java
@@ -0,0 +1,34 @@
+package com.nintech.chapatamod.items;
+
+import com.nintech.chapatamod.ChapataMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class CreativeTabInit {
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ChapataMod.MODID);
+
+    public static final List<Supplier<? extends ItemLike>> CHAPATA_TAB_ITEMS = new ArrayList<>();
+
+    public static final RegistryObject<CreativeModeTab> CHAPATA_TAB = TABS.register("chapata_tab",
+            ()-> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.chapata_tab"))
+                    .icon(ChapataItem.CHAPATA.get()::getDefaultInstance)
+                    .displayItems((displayParams, output)->{
+                       CHAPATA_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get()));
+                    })
+                    .build()
+    );
+    public static <T extends Item> RegistryObject<T> addToTab(RegistryObject<T> itemLike){
+        CHAPATA_TAB_ITEMS.add(itemLike);
+        return itemLike;
+    }
+}
