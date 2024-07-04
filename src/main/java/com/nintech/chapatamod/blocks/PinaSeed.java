e7c027a7ac878c2c4847b7b03e90ad7c3276abb9
NeftaliGC <aliguiacruz@outlook.com>
2024-07-03 22:43:22 -0600

    json etc

---
+++ src/main/java/com/nintech/chapatamod/blocks/PinaSeed.java
@@ -0,0 +1,9 @@
+package com.nintech.chapatamod.blocks;
+
+import net.minecraft.world.level.block.CropBlock;
+
+public class PinaSeed extends CropBlock {
+    public PinaSeed(Properties properties) {
+        super(properties);
+    }
+}
